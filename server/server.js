d328a14046d3a7e3c080a6d40b526b82b6ef35f3
thisuserwasreplacedbyai <hubkaniklas@gmail.com>
2025-11-17 16:25:44 +0100

    prepped server.js with middleware for json and cookies

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 require('dotenv').config();
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const connectDB = require('./config/db');
 
 const app = express();
 
 // connect to database
 connectDB();
+
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 
 app.get('/api/health', (req, res) => {
   res.json({ status: 'server is running' });
